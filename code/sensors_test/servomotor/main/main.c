12ca643cdf1f50bcb7b074bfefe38ee84b66ef31
Keyneees <michele.nicoletti2000@gmail.com>
2024-04-15 16:06:46 +0200

    Added directory for servomotor test

---
+++ code/sensors_test/servomotor/main/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+
+#define APP "SERVOMOTOR_TEST"
+//FILE FOR SERVOMOTOR TEST
+
+void app_main(void){
+    
+}
