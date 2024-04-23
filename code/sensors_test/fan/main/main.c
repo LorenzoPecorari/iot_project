9c13398d53fd2b775bd2febcd0c4bb3a88267526
Keyneees <michele.nicoletti2000@gmail.com>
2024-04-23 22:09:29 +0200

    Working fan test

--- code/sensors_test/fan/main/main.c
+++ code/sensors_test/fan/main/main.c
@@ -1,49 +1,34 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "driver/adc.h"
-#include "esp_system.h"
 
-// Definizione dei pin
-#define PIN_TRANSISTOR GPIO_NUM_2
-#define PIN_POTENZIOMETRO ADC_CHANNEL_6 // Pin ADC 6 corrispondente al GPIO 34 su ESP32-S3
+#define PIN_TRANSISTOR GPIO_NUM_2 // Imposta il pin collegato alla base del transistor
+#define VENTOLA_PIN GPIO_NUM_4 // Imposta il pin di controllo dell'ESP32-S3
 
 void init() {
-    // Configurazione del pin del transistor
-    // gpio_config_t transistorConfig;
-    // transistorConfig.pin_bit_mask = (1ULL << PIN_TRANSISTOR);
-    // transistorConfig.mode = GPIO_MODE_OUTPUT;
-    // gpio_config(&transistorConfig);
-
-    // // Configurazione del pin del potenziometro come ingresso analogico
-    // adc1_config_width(ADC_WIDTH_BIT_12);
-    // adc1_config_channel_atten(PIN_POTENZIOMETRO, ADC_ATTEN_DB_0);
+    // Configura i pin
     gpio_set_direction(PIN_TRANSISTOR, GPIO_MODE_OUTPUT);
+    gpio_set_direction(VENTOLA_PIN, GPIO_MODE_OUTPUT);
 }
 
-void controlloTrasistor(void *pvParameter) {
+void controlloVentola(void *pvParameter) {
     while (1) {
-        // Accendi il transistor
+        // Accendi la ventola
         gpio_set_level(PIN_TRANSISTOR, 1);
-        printf("Pin %d\n", gpio_get_level(PIN_TRANSISTOR));
-        // Leggi il potenziale in uscita
-        // uint32_t potenzialeUscita = adc1_get_raw(PIN_POTENZIOMETRO);
-        vTaskDelay(5000/portTICK_PERIOD_MS);
-        // // Stampa il potenziale in uscita sulla porta seriale
-        // printf("Potenziale in uscita: %lu\n", potenzialeUscita);
-
-        // Spegni il transistor
+        gpio_set_level(VENTOLA_PIN, 1); // Accendi il pin di controllo
+        vTaskDelay(5000 / portTICK_PERIOD_MS); // Attendi per 5 secondi
+        
+        // Spegni la ventola
         gpio_set_level(PIN_TRANSISTOR, 0);
-        printf("Pin %d\n", gpio_get_level(PIN_TRANSISTOR));
-        // Attendi un po' prima di ripetere il loop
-        vTaskDelay(5000/portTICK_PERIOD_MS);
+        gpio_set_level(VENTOLA_PIN, 0); // Spegni il pin di controllo
+        vTaskDelay(5000 / portTICK_PERIOD_MS); // Attendi per 5 secondi
     }
 }
 
 void app_main() {
-    // Inizializzazione delle periferiche
+    // Inizializza i pin
     init();
 
-    // Creazione del task per il controllo del transistor
-    xTaskCreate(controlloTrasistor, "Controllo Transistor", 4096, NULL, 5, NULL);
+    // Crea il task per il controllo della ventola
+    xTaskCreate(controlloVentola, "Controllo Ventola", 4096, NULL, 5, NULL);
 }
