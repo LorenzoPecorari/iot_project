3e880fbfe9f3fcf1e97ac05be7f02255bd1a5662
Keyneees <michele.nicoletti2000@gmail.com>
2024-04-23 22:59:03 +0200

    Added led

--- code/sensors_test/fan/main/main.c
+++ code/sensors_test/fan/main/main.c
@@ -8,16 +8,18 @@
 *ESP32S3* -|- GPIO2 --- CABLE --- RESISTENCE 10 KOHM --- CABLE ------------------------------------ BASE -|- *TRANSISTOR* 
            |                                                                                              |
            |- GND --- CABLE -------------------------------------------------------------------- EMITTER -|
-
+           |   |-----------------------------------------------------|
+           |                                                         |
+           |- GPIO4 --- CABLE --- POSITIVE --- *LED* --- NEGATIVE ---|
 */
 
 #define PIN_TRANSISTOR GPIO_NUM_2 // Imposta il pin collegato alla base del transistor
-#define VENTOLA_PIN GPIO_NUM_4 // Imposta il pin di controllo dell'ESP32-S3
+#define LED_PIN GPIO_NUM_4 // Imposta il pin di controllo dell'ESP32-S3
 
 void init() {
     // Configura i pin
     gpio_set_direction(PIN_TRANSISTOR, GPIO_MODE_OUTPUT);
-    // gpio_set_direction(VENTOLA_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 }
 
 void controlloVentola(void *pvParameter) {
@@ -25,13 +27,13 @@ void controlloVentola(void *pvParameter) {
         // Accendi la ventola
         printf("Spenta\n");
         gpio_set_level(PIN_TRANSISTOR, 1);
-        // gpio_set_level(VENTOLA_PIN, 1); // Accendi il pin di controllo
+        gpio_set_level(LED_PIN, 0); // Accendi il pin di controllo
         vTaskDelay(5000 / portTICK_PERIOD_MS); // Attendi per 5 secondi
         
         // Spegni la ventola
         printf("Accesa\n");
         gpio_set_level(PIN_TRANSISTOR, 0);
-        // gpio_set_level(VENTOLA_PIN, 0); // Spegni il pin di controllo
+        gpio_set_level(LED_PIN, 1); // Spegni il pin di controllo
         vTaskDelay(5000 / portTICK_PERIOD_MS); // Attendi per 5 secondi
     }
 }
