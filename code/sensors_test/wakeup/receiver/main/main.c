f1c44cb05694934627bf2dc1048404fb80b18b5a
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2024-05-28 14:57:22 +0200

    not working wake up

--- code/sensors_test/wakeup/receiver/main/main.c
+++ code/sensors_test/wakeup/receiver/main/main.c
@@ -1,13 +1,27 @@
 #include "new_esp-now_utils.h"
 
-void app_main(void){
-    
-    esp_now_utils_handle_error(nvs_flash_init());
+void app_main(void) {
+    // Inizializza NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // Inizializza Wi-Fi
     wifi_init();
+
+    // Inizializza ESP-NOW
     init_esp_now();
 
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    // Entra in modalità light sleep
+    ESP_LOGI(APP_NAME, "Entering light sleep mode");
     esp_light_sleep_start();
-    send_mac();
 
+    // Questo punto viene raggiunto solo al risveglio
+    ESP_LOGI(APP_NAME, "Woke up from light sleep");
+
+    // Eventuali azioni da eseguire al risveglio
+    // ...
 }
--- code/sensors_test/wakeup/sender/main/main.c
+++ code/sensors_test/wakeup/sender/main/main.c
@@ -1,11 +1,25 @@
-#include "new_esp-now.h"
+#include "new_esp-now_utils.h"
 
-void app_main(void){
-    
-    esp_now_utils_handle_error(nvs_flash_init());
+void app_main(void) {
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    // Initialize Wi-Fi and ESP-NOW
     wifi_init();
     init_esp_now();
 
+    // Send MAC address (optional, for testing)
     send_mac();
-    send_message("MSG", "It's time to push this code!");
+
+    // Enter light sleep mode
+    ESP_LOGI(APP_NAME, "Entering light sleep mode");
+    esp_light_sleep_start();
+
+    // This point is reached only upon wake up
+    ESP_LOGI(APP_NAME, "Woke up from light sleep");
 }
