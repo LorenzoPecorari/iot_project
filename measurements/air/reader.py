51181f5fb32685cd8ebe5c6922a11f32565a58de
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2024-05-30 17:28:39 +0200

    [measurements] plots + data for interm pres

--- measurements/air/reader.py
+++ measurements/air/reader.py
@@ -7,13 +7,13 @@ s = serial.Serial('/dev/ttyUSB0', baudrate)
 index = 0
 
 try:
-    file_to_write = open(filename, mode='w')
-    while (index <= 1200):
-        line = s.readline().decode().strip()
-        print("Data:", line)
+	file_to_write = open(filename, mode='w')
+	while (i < 600):
+		line = s.readline().decode().strip()
+		print("Data:", line)
         
-        file_to_write.write(f"{index},{line}\n")
-        index += 1
+		file_to_write.write(f"{index},{line}\n")
+		index += 1
 
 except KeyboardInterrupt:
     print("Aborted.")
