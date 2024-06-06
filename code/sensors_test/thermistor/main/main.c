3495904a9bffd15ccd6f194758bb800810434756
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2024-06-06 21:37:48 +0200

    [thermistor] last comments

--- code/sensors_test/thermistor/main/main.c
+++ code/sensors_test/thermistor/main/main.c
@@ -21,6 +21,8 @@
     - beta coefficient is found experimentally, usually is around 3900 and 4000; tells about sensivity of thermistor
     - at 25 °C (nominal temperature), resistance of thermistor is approximately 10k Ohm
 
+    - resistance of thermistor wrt sampled voltage: R_thermistor = R_ext * (Vcc / V_sampled - 1)    <-- tension divisor!!! 
+
     - Beta function for finding temperature:
         1 / T = 1 / t_s + (ln(R_thermistor / external resistance) / beta_coefficient) =>
             => T = 1 / (1 / t_s + (ln(R_thermistor / external resistance) / beta_coefficient))
@@ -32,7 +34,7 @@
 #define V_REF 1100
 #define R_FIXED 100000              // outer resistance, 100k Ohm
 #define B_COEFFICIENT 3950          // Beta coefficient thermistor
-#define NOMINAL_RESISTANCE 10000    // Nominal resistance of thermistor
+#define NOMINAL_RESISTANCE 10000    // Nominal resistance of thermistor, 10k Ohm
 #define NOMINAL_TEMPERATURE 25
 
 static esp_adc_cal_characteristics_t adc1_chars;
@@ -53,9 +55,11 @@ void app_main(void)
         voltage = adc1_get_raw(CHANNEL);
         voltage = esp_adc_cal_raw_to_voltage(voltage, &adc1_chars);
 
-        resistance = (float)R_FIXED * ((3300.0 / voltage) - 1);             // resistance of thermistor
+        // resistance of thermistor
+        resistance = (float)R_FIXED * ((3300.0 / voltage) - 1);
         printf("R_thermistor: %.2f ohm\n", resistance);
 
+        // temperature evaluated in Celsius
         temperature = 1.0 / (1.0 / (NOMINAL_TEMPERATURE + 273.15) + log(resistance / NOMINAL_RESISTANCE) / B_COEFFICIENT) - 273.15;
 
         printf("T: %.2f°C\n", temperature);
